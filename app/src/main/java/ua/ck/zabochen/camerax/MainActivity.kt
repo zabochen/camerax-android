b35e02dcbd91ca9ddd65d9231df91ae957c7d47d
zabochen <zabochen@gmail.com>
2020-06-17 17:12:58 +0300

    Init project.

---
+++ app/src/main/java/ua/ck/zabochen/camerax/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.ck.zabochen.camerax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
